5fda1ce9641d6dccb18ba746ce26e148bf3e8c10
mmelko <mmelko@redhat.com>
2024-12-02 19:30:13 +0100

    fix(xml-camel-resource-serializer): Fix imports to prevent circular depednencies error

--- packages/ui/src/serializers/xml-camel-resource-serializer.ts
+++ packages/ui/src/serializers/xml-camel-resource-serializer.ts
@@ -1,7 +1,10 @@
+import { CamelResourceSerializer } from './camel-resource-serializer';
 import { isXML, XmlParser } from './xml/xml-parser';
-import { XmlConverter } from './xml/xml-converter';
-import { CamelResource, sourceSchemaConfig, SourceSchemaType } from '../models/camel';
+import { sourceSchemaConfig } from '../models/camel/source-schema-config';
+import { SourceSchemaType } from '../models/camel/source-schema-type';
+import { CamelResource } from '../models/camel/camel-resource';
 import { JSONSchema4 } from 'json-schema';
+import { XmlConverter } from './xml/xml-converter';
 import { formatXml } from './xml/xml-utils';
 import { CamelResourceSerializer } from './camel-resource-serializer';
 import { CamelYamlDsl, Integration, Kamelet, KameletBinding, Pipe } from '@kaoto/camel-catalog/types';
@@ -10,20 +13,21 @@ export class XmlCamelResourceSerializer implements CamelResourceSerializer {
   getLabel(): string {
     return 'XML';
   }
+
+  static isApplicable(code: unknown): boolean {
+    return isXML(code as string);
+  }
   xmlSerializer: XMLSerializer = new XMLSerializer();
-  xmlConverter: XmlConverter;
-  xmlParser: XmlParser;
 
   parse(code: unknown): CamelYamlDsl | Integration | Kamelet | KameletBinding | Pipe {
     this.xmlParser = new XmlParser(sourceSchemaConfig.config[SourceSchemaType.Route].schema?.schema as JSONSchema4);
     const entities = this.xmlParser.parseXML(code as string);
-
     return entities;
   }
 
   serialize(resource: CamelResource): string {
-    this.xmlConverter = new XmlConverter();
-    const xmlDocument = this.xmlConverter.generateXmlDocument(resource);
+    const visualEntities = resource.getVisualEntities().map((e) => e as unknown as { entityDef: any });
+    const xmlDocument = new XmlConverter().generateXmlDocument(visualEntities);
     return formatXml(this.xmlSerializer.serializeToString(xmlDocument));
   }
 
