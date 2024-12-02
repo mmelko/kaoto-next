d44eac608e33af0033c75e5022d38082259d19f6
mmelko <mmelko@redhat.com>
2024-12-02 19:30:13 +0100

    fix(xml-camel-resource-serializer): Fix imports to prevent circular depednencies error

--- packages/ui/src/serializers/xml-camel-resource-serializer.ts
+++ packages/ui/src/serializers/xml-camel-resource-serializer.ts
@@ -1,28 +1,32 @@
-import { isXML, XmlParser } from './xml/xml-parser';
-import { XmlConverter } from './xml/xml-converter';
-import { CamelResource, sourceSchemaConfig, SourceSchemaType } from '../models/camel';
-import { JSONSchema4 } from 'json-schema';
-import { formatXml } from './xml/xml-utils';
 import { CamelResourceSerializer } from './camel-resource-serializer';
+import { isXML, XmlParser } from './xml/xml-parser';
+import { sourceSchemaConfig } from '../models/camel/source-schema-config';
+import { SourceSchemaType } from '../models/camel/source-schema-type';
+import { CamelResource } from '../models/camel/camel-resource';
+import { JSONSchema4 } from 'json-schema';
+import { XmlConverter } from './xml/xml-converter';
+import { formatXml } from './xml/xml-utils';
 
 export class XmlCamelResourceSerializer implements CamelResourceSerializer {
   getLabel(): string {
     return 'XML';
   }
+
   static isApplicable(code: unknown): boolean {
     return isXML(code as string);
   }
   xmlSerializer: XMLSerializer = new XMLSerializer();
 
-  parse(code: unknown): unknown {
+  parse(code: string): unknown {
     const xmlParser = new XmlParser(sourceSchemaConfig.config[SourceSchemaType.Route].schema?.schema as JSONSchema4);
-    const entities = xmlParser.parseXML(code as string);
 
+    const entities = xmlParser.parseXML(code as string);
     return entities;
   }
 
   serialize(resource: CamelResource): string {
-    const xmlDocument = new XmlConverter().generateXmlDocument(resource);
+    const visualEntities = resource.getVisualEntities().map((e) => e as unknown as { entityDef: any });
+    const xmlDocument = new XmlConverter().generateXmlDocument(visualEntities);
     return formatXml(this.xmlSerializer.serializeToString(xmlDocument));
   }
 
