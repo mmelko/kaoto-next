59d15224c171c1a210fe9e64d61cb89162269ce0
mmelko <mmelko@redhat.com>
2025-01-16 15:33:19 +0100

    test(xml): Add test that parses XML and YAML from the folders and compares

---
+++ packages/ui/src/serializers/xml-to-yaml.test.ts
@@ -0,0 +1,48 @@
+import catalogLibrary from '@kaoto/camel-catalog/index.json';
+import { getFirstCatalogMap } from '../stubs/test-load-catalog';
+import { CatalogLibrary } from '@kaoto/camel-catalog/types';
+import { CamelCatalogService, CatalogKind } from '../models';
+import fs from 'fs';
+import path from 'path';
+import { YamlCamelResourceSerializer } from './yaml-camel-resource-serializer';
+import { XmlParser } from './xml/xml-parser';
+
+describe('XmlParser - XML to YAML comparison', () => {
+  let parser: XmlParser;
+  let yamlSerializer: YamlCamelResourceSerializer;
+
+  beforeAll(async () => {
+    parser = new XmlParser();
+    yamlSerializer = new YamlCamelResourceSerializer(); // Initialize your YAML serializer
+    const catalogsMap = await getFirstCatalogMap(catalogLibrary as CatalogLibrary);
+    CamelCatalogService.setCatalogKey(CatalogKind.Processor, catalogsMap.modelCatalogMap);
+  });
+
+  const xmlDir = path.join(__dirname, '../stubs/xml');
+  const yamlDir = path.join(__dirname, '../stubs/yaml');
+
+  const xmlFiles = fs.readdirSync(xmlDir).filter((file) => file.endsWith('.xml'));
+
+  const tesXmlToYaml = (xmlFile: string) => {
+    const xmlFilePath = path.join(xmlDir, xmlFile);
+    const yamlFilePath = path.join(yamlDir, xmlFile.replace('.xml', '.yaml'));
+
+    const xmlContent = fs.readFileSync(xmlFilePath, 'utf-8');
+    const expectedYamlContent = fs.readFileSync(yamlFilePath, 'utf-8');
+    const expectedYaml = yamlSerializer.parse(expectedYamlContent); // Use your YAML serializer
+    const result = parser.parseXML(xmlContent);
+
+    expect(result).toEqual(expectedYaml);
+  };
+
+  xmlFiles.forEach((xmlFile) => {
+    it(`parses and compares ${xmlFile} correctly`, () => {
+      tesXmlToYaml(xmlFile);
+    });
+  });
+
+  // it('parse single file correctly', () => {
+  //   const fileName = 'routeConfiguration.xml';
+  //   testFile(fileName);
+  // });
+});
