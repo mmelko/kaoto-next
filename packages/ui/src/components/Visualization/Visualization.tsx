4a10bdb4235c3995dd3ca6ce457687ad7e77e01a
mmelko <mmelko@redhat.com>
2025-08-07 12:13:13 +0200

    fix:WIP: fix icon and onhover toolbar

--- packages/ui/src/components/Visualization/Visualization.tsx
+++ packages/ui/src/components/Visualization/Visualization.tsx
@@ -2,10 +2,11 @@ import { CanvasFormTabsProvider } from '@kaoto/forms';
 import { FunctionComponent, JSX, PropsWithChildren, ReactNode } from 'react';
 import { BaseVisualCamelEntity } from '../../models/visualization/base-visual-entity';
 import { ErrorBoundary } from '../ErrorBoundary';
-import { Canvas } from './Canvas';
+
 import { CanvasFallback } from './CanvasFallback';
 import { ContextToolbar } from './ContextToolbar/ContextToolbar';
 import './Visualization.scss';
+import { RFCanvas } from './ReactFlowCanvas/RFCanvas';
 
 interface CanvasProps {
   className?: string;
@@ -19,7 +20,7 @@ export const Visualization: FunctionComponent<PropsWithChildren<CanvasProps>> = 
     <div className={`canvas-surface ${props.className ?? ''}`}>
       <CanvasFormTabsProvider>
         <ErrorBoundary fallback={props.fallback ?? <CanvasFallback />}>
-          <Canvas
+          <RFCanvas
             contextToolbar={<ContextToolbar additionalControls={props.additionalToolbarControls} />}
             entities={props.entities}
           />
